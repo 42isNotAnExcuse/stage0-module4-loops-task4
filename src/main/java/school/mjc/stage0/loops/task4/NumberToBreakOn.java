556d14df6f7fe88ee11d4b62d155d91c8501ca3e
Igor Bugaiov <no-reply>
2022-10-20 19:18:42 +0300

    st0-mod4.2-t4 1st try

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -1,7 +1,22 @@
 package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
+    // Write a program that will consume 2 args:
+    // the first is number to break on,
+    // the second to iterate till,
+    // if number to iterate till is less than to break with
+    // num2it < breOn -> sout("iterating till the end")
+    // and print all the numbers till the end,
+    // otherwise all numbers till toBreakWith.
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
+        int stop = toBreakWith;
 
+        if (numberToGoUntil < stop) {
+            System.out.println("iterating till the end");
+            stop = numberToGoUntil;
+        }
+        for (int i = 1; i <= stop; i++) {
+            System.out.println(i);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -1,7 +1,39 @@
 package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
+    // Write a program that will consume 2 args:
+    // the first is number to skip, the second to iterate till.
+    // You should consider the cases:
+    // 3.1. when numberToSkip is greater than lastInRow
+    // -> output is:"number to skip is bugger then the last"
+    // 3.2. when lastInRow is negative
+    // -> output is:"last number in row is negative"
+    // 3.3. for the rest cases
+    // -> output is: "skipped sum is number" "counted sum is number",
+    // where number is an actual sum of them:
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
+        String large = "number to skip is bugger then the last";
+        String neg = "last number in row is negative";
+        String skSum = "skipped sum is %d";
+        String cdSum = "counted sum is %d";
+        int sk = 0;
+        int cd = 0;
 
+        if (numberToSkip > lastInRow) {
+            System.out.println(large);
+            return;
+        } else if (lastInRow < 0) {
+            System.out.println(neg);
+            return;
+        }
+        for (int i = 0; i <= lastInRow; i++) {
+            if (i <= numberToSkip) {
+                sk += i;
+            } else {
+                cd += i;
+            }
+        }
+        System.out.printf(skSum + "%n", sk);
+        System.out.printf(cdSum + "%n", cd);
     }
 }
